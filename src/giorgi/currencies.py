a5d0ffaa1875bddfec7c643c25235f0cee2e7922
Bram Rooseleer <bram.rooseleer@gmail.com>
2024-12-25 12:39:36 +0100

    added most used currencies

--- src/giorgi/currencies.py
+++ src/giorgi/currencies.py
@@ -18,4 +18,14 @@ from giorgi.quantities import BaseQuantityType
 
 
 EUR = BaseQuantityType('Euro', unit_symbol='€', unit_name='Euro')
-USD = BaseQuantityType('US dollar', unit_symbol='US$', unit_name='US dollar')
+GBP = BaseQuantityType('British Pound', unit_symbol='£', unit_name='British Pound')
+JPY = BaseQuantityType('Japanese Yen', unit_symbol='¥', unit_name='Japanese Yen')
+CNY = BaseQuantityType('Chinese Yuan', unit_symbol='CN¥', unit_name='Chinese Yuan')
+CHF = BaseQuantityType('Swiss Franc', unit_symbol='fr.', unit_name='Swiss Franc')
+SEK = BaseQuantityType('Swedish Krona', unit_symbol='kr', unit_name='Swedish Krona')
+USD = BaseQuantityType('US Dollar', unit_symbol='$', unit_name='US Dollar')
+CAD = BaseQuantityType('Canadian Dollar', unit_symbol='CA$', unit_name='Canadian Dollar')
+AUD = BaseQuantityType('Australian Dollar', unit_symbol='AU$', unit_name='Australian Dollar')
+NZD = BaseQuantityType('New Zealand Dollar', unit_symbol='NZ$', unit_name='New Zealand Dollar')
+SGD = BaseQuantityType('Singapore Dollar', unit_symbol='S$', unit_name='Singapore Dollar')
+HKD = BaseQuantityType('Hong Kong Dollar', unit_symbol='HK$', unit_name='Hong Kong Dollar')
