4595d08adc3a93db0d1417e92ecb4a23d1db03a8
Bram Rooseleer <bram.rooseleer@gmail.com>
2024-12-25 15:34:06 +0100

    added bias to units

--- src/giorgi/quantities.py
+++ src/giorgi/quantities.py
@@ -21,14 +21,21 @@ from giorgi.units import Unit
 
 class Quantity:
     
-    def __init__(self, value=0, **kwargs):
-        self.value = value + sum(self.units[symbol].scale*value for symbol, value in kwargs.items())
+    def __init__(self, value=0, symbol_or_name=None, **kwargs):
+        self.value = self.unit(symbol_or_name).to_main_unit(value) + \
+            sum(self.unit(symbol_or_name).to_main_unit(value) for symbol_or_name, value in kwargs.items())
+
+    @classmethod
+    def unit(cls, name_or_symbol):
+        if name_or_symbol is None:
+            return cls.main_unit
+        return cls._units[name_or_symbol]
 
     def __hash__(self):
         return hash((self.value, type(self)))
     
     def to_string(self, unit: Unit, float_fmt: str='.6f') -> str:
-        return f"{self.value/unit.scale:{float_fmt}} {unit.symbol}"
+        return f"{unit.from_main_unit(self.value):{float_fmt}}{'' if unit.no_space_before_unit else ' '}{unit.symbol}"
 
     def __repr__(self) -> str:
         return self.to_string(unit=self.main_unit)
@@ -37,8 +44,14 @@ class Quantity:
         return self.to_string(unit=self.main_unit, float_fmt='.3f')
     
     def __format__(self, fmt: str) -> str:
-        float_fmt, unit_symbol = re.match('(\d*\.\d*)(.+)', fmt).groups()
-        unit = self.units[unit_symbol]
+        for unit_symbol in sorted(self._units, key=lambda symbol: -len(symbol)):
+            if fmt.endswith(unit_symbol):
+                unit = self.unit(unit_symbol)
+                float_fmt = fmt[:-len(unit_symbol)]
+                break
+        else:
+            unit = self.main_unit
+            float_fmt = fmt
         return self.to_string(unit=unit, float_fmt=float_fmt)
     
     def __neg__(self) -> Self:
@@ -47,8 +60,6 @@ class Quantity:
     def __add__(self, other: Self) -> Self:
         if other == 0:
             return self
-        if isinstance(other, (int, float)):
-            other = Ratio(other)
         if type(self) != type(other):
             raise TypeError("Can only add quantities of same type together")
         return type(self)(self.value + other.value)
@@ -80,7 +91,7 @@ class Quantity:
     def __rtruediv__(self, other: int | float) -> Self:
         if not isinstance(other, (int, float)):
             raise TypeError()
-        return Ratio(other)/self
+        return other/self
     
     def __div__(self, other: int | float | Self) -> Self:
         if type(self) != type(other):
@@ -124,10 +135,13 @@ class QuantityType(type):
     def _hashable_base_quantities(base_quantities):
         return tuple((base_quantity, exponent) for base_quantity, exponent in sorted(base_quantities.items(), key=lambda kv: str(kv[0])) if exponent)
     
-    def __new__(cls, name, *args, **kwargs):
+    def __new__(cls, name, base_quantities, *args, **kwargs):
+        if base_quantities == {}:
+            return float
         return super().__new__(cls, name, (Quantity,), {})
 
     def make(base_quantities):
+        base_quantities = {k: v for k, v in base_quantities.items() if v}
         hashable_base_quantities = QuantityType._hashable_base_quantities(base_quantities)
         try:
             return QuantityType.existing[hashable_base_quantities]
@@ -146,7 +160,7 @@ class QuantityType(type):
         self.name = name
         self.base_quantities = base_quantities        
         hashable_base_quantities = QuantityType._hashable_base_quantities(base_quantities)
-        self.units = {}
+        self._units = {}
         if unit_symbol is None:
             unit_symbol = '×'.join(f"{base_quantity.main_unit.symbol}{exponent_superscript(exponent)}" for base_quantity, exponent in hashable_base_quantities)
             unit_scale = reduce(lambda a, b: a*b, (base_quantity.main_unit.scale**exponent for base_quantity, exponent in hashable_base_quantities))
@@ -183,10 +197,15 @@ class QuantityType(type):
         return QuantityType._hashable_base_quantities(self.base_quantities) == QuantityType._hashable_base_quantities(other.base_quantities)
     
     def _add_unit(self, unit):
-        self.units[unit.symbol] = unit
+        self._units[unit.symbol] = unit
+        if unit.name:
+            self._units[unit.name.replace(' ', '_')] = unit
 
 
 class BaseQuantityType(QuantityType):
+        
+    def __new__(cls, name, **kwargs):
+        return super().__new__(cls, name, None, **kwargs)
 
     def __init__(self, name, *args, **kwargs):
         super().__init__(name, {self: 1},  *args, **kwargs)
