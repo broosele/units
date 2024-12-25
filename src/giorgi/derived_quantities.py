ccc6b3918aa91928cc95217e12ba3e4ba76d2972
Bram Rooseleer <bram.rooseleer@gmail.com>
2024-12-25 12:39:06 +0100

    added magnetic quantities

--- src/giorgi/derived_quantities.py
+++ src/giorgi/derived_quantities.py
@@ -15,7 +15,7 @@ from .base_quantities import Length, Time, Mass, Temperature, Current, Amount, L
 
 
 # Geometric quantities
-Ratio = QuantityType('Ratio', {}, '', prefices=[UNARY_PREFIX])
+# Ratio = QuantityType('Ratio', {}, '', prefices=[UNARY_PREFIX])
 Area = QuantityType('Area', {Length: 2})
 Volume = QuantityType('Volume', {Length: 3})
 
@@ -33,9 +33,11 @@ Power = QuantityType('Power', {Mass: 1, Length: 2, Time: -3}, 'W', 'watt')
 Charge = QuantityType('Charge', {Current: 1, Time: 1}, 'C', 'coulomb'),
 Voltage = QuantityType('Voltage', {Mass: 1, Length: 2, Time: -3, Current: -1}, 'V', 'volt')
 Resistance = QuantityType('Resistance', {Mass: 1, Length: 2, Time: -3, Current: -2}, 'Ω','ohm' )
-Conductance = QuantityType('Conductance', {Mass: -1, Length: -2, Time: 3, Current: 2}, 'S','Siemens' )
+Conductance = QuantityType('Conductance', {Mass: -1, Length: -2, Time: 3, Current: 2}, 'S','siemens' )
 Capacitance = QuantityType('Capacitance', {Mass: -1, Length: -2, Time: 4, Current: 2}, 'F', 'farad')
 Inductance = QuantityType('Inductance', {Mass: 1, Length: 2, Time: -2, Current: -2}, 'H', 'henri')
+MagneticFlux = QuantityType('Magnetic flux', {Mass: 1, Length: 2, Time: -2, Current: -1}, 'Wb', 'weber')
+MagneticFluxDensity = QuantityType('Magnetic flux density', {Mass: 1, Time: -2, Current: -1}, 'T', 'tesla')
 
 
 # Other
